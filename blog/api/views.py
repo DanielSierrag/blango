3ead13200a6c9cd806f176ef90964094e1fd3bf4
Daniel Greco <danielgreco2209@gmail.com>
2025-04-25 21:34:35 +0000

    Finish permissions

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,5 +1,8 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 from rest_framework.authentication import TokenAuthentication
+
+from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 
 from blog.models import Post
 from blog.api.serializers import PostSerializer
@@ -7,10 +10,12 @@ from blog.api.serializers import PostSerializer
 
 class PostListCreateView(generics.ListCreateAPIView):
   authentication_classes = [TokenAuthentication]
+  permission_classes = [IsAuthenticatedOrReadOnly]
   queryset = Post.objects.all()
   serializer_class = PostSerializer
 
 
 class PostRetieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
   queryset = Post.objects.all()
   serializer_class = PostSerializer
