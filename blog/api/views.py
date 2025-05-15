4a44b080fdbe1d0f14550b1696f9f26f71530d03
Daniel Greco <danielgreco2209@gmail.com>
2025-05-15 15:33:38 +0000

    Finish caching

--- blog/api/views.py
+++ blog/api/views.py
@@ -3,6 +3,11 @@ from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
+
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers, vary_on_cookie
 
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 
@@ -20,10 +25,25 @@ class PostViewSet(viewsets.ModelViewSet):
   permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
   queryset = Post.objects.all()
 
+  @method_decorator(cache_page(120))
+  def list(self, *args, **kwargs):
+    return super(PostViewSet, self).list(*args, **kwargs)
+
   def get_serializer_class(self):
     if self.action in ("list", "create"):
       return PostSerializer
     return PostDetailSerializer
+
+  @method_decorator(cache_page(300))
+  @method_decorator(vary_on_headers('Authorization'))
+  @method_decorator(vary_on_cookie)
+  @action(methods=['get'], detail=False, name='Post by the logged in user')
+  def mine(self, request):
+    if not request.user.is_authenticated:
+      raise PermissionDenied('You must be logged in to se which Posts are yours')
+    posts = self.get_queryset().filter(author=request.user)
+    serialzer = self.get_serializer_class()(many=True, context={'request': request})
+    return Response(serialzer.data)
 
 
 class UserDetailView(generics.RetrieveAPIView):
@@ -31,10 +51,22 @@ class UserDetailView(generics.RetrieveAPIView):
   serializer_class = UserSerializer
   lookup_field = 'email'
 
+  @method_decorator(cache_page(300))
+  def get(self, *args, **kwargs):
+    return super(UserDetailView, self).get(*args, **kwargs)
+
 
 class TagViewSet(viewsets.ModelViewSet):
   serializer_class = TagSerializer
   queryset = Tag.objects.all()
+
+  @method_decorator(cache_page(300))
+  def list(self, *args, **kwargs):
+    return super(TagViewSet, self).list(*args, **kwargs)
+
+  @method_decorator(cache_page(300))
+  def retrieve(self, *args, **kwargs):
+    return super(TagViewSet, self).retrieve(*args, **kwargs)
 
   @action(methods=["get"], detail=True, name="Posts with the Tag")
   def posts(self, request, pk=None):
